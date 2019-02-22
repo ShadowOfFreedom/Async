241e80b2a6f1586af73647108220d30d84d62866
Wojciech Wojtkielewicz <MJBR64@motorolasolutions.com>
2019-02-22 10:26:29 +0100

    add mising files

---
+++ Zadanie_01/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zadanie_01
+{
+    public static class Worker
+    {
+        public static async Task<bool> DoWorkAsync(int n)
+        {
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < n; i++)
+            {
+                string path = Path.Combine(@"C:\Loggs\", $"{i}.log");
+                tasks.Add(new Task(()=>File.Create(path)));
+            }
+
+            tasks.ForEach(t =>
+            {
+                t.Start();
+                Thread.Sleep(1000);
+            });
+
+            await Task.WhenAll(tasks);
+
+            return true;
+        }
+    }
+}
